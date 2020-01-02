c7ccd86ca6c9075bd6512576aa63c051058d12f7
Nick Heiner <nheiner@netflix.com>
2020-01-02 12:55:23 -0800

    Fix type errors

--- bin/index.js
+++ bin/index.js
@@ -7,18 +7,21 @@ const {argv} = require('yargs')
     rule: {
       alias: 'r',
       array: true,
-      description: 'The rule to disable. Pass this flag multiple times to disable multiple rules at once.'
+      demandOption: true,
+      description: 'The rule to disable. Pass this flag multiple times to disable multiple rules at once.',
+      coerce: rules => rules.map(/** @param {any} rule */ rule => rule.toString())
     }
-  })
-  .demandOption('rule', 'You must specify at least one eslint rule to disable.');
+  });
 
 // TODO: add ability to specify an eslint instance / command path
 // Possibly add ability to pass through arbitrary other args to eslint?
 
 async function main() {
   try {
+    console.log(argv)
     await eslintBankruptcy({
       files: argv._,
+      // @ts-ignore the coerce function ensures that argv.rule will be string[].
       rules: argv.rule
     });
   } catch (e) {
